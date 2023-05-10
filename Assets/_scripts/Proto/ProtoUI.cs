c721826fb55feeb5ae642476a99f26361361e74b
paragonusaxis <paragonusaxis@gmail.com>
2023-05-10 11:28:33 +0200

    Core UI done

--- Assets/_scripts/Proto/ProtoUI.cs
+++ Assets/_scripts/Proto/ProtoUI.cs
@@ -8,8 +8,17 @@ using TMPro;
 
 public class ProtoUI : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public GameObject playMenu;
+
     public TextMeshProUGUI textTime;
     private float timer;
+    private float minutes;
+    private float seconds;
+    private string stringMinutes;
+    private string stringSeconds;
+
+    public static bool isPaused = false;
 
     private void Update()
     {
@@ -20,14 +29,59 @@ public class ProtoUI : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        float minutes = Mathf.FloorToInt(timer / 60);
-        float seconds = Mathf.FloorToInt(timer % 60);
+        minutes = Mathf.FloorToInt(timer / 60);
+        seconds = Mathf.FloorToInt(timer % 60);
 
-        textTime.text = minutes + ":" + seconds;
+        if (minutes < 10)
+        {
+            stringMinutes = "0" + minutes;
+        } else
+        {
+            stringMinutes = minutes.ToString();
+        }
+
+        if (seconds < 10)
+        {
+            stringSeconds = "0" + seconds;
+        }
+        else
+        {
+            stringSeconds = seconds.ToString();
+        }
+
+        textTime.text = stringMinutes + ":" + stringSeconds;
     }
 
     public void RefreshScene()
     {
         SceneManager.LoadScene(0);
+        PauseGame();
+    }
+
+    public void PauseGame()
+    {
+        if (isPaused) 
+        {
+            isPaused = false;   
+            Resume();
+        } else
+        {
+            isPaused = true;
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        playMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void Resume()
+    {
+        playMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
