89c75ee2ba482e8e33f664343af49db6be3212d0
paragonusaxis <paragonusaxis@gmail.com>
2023-05-10 16:22:33 +0200

    barebones end menu

---
+++ Assets/_scripts/EndMenu/EndMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndMenu : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        // Go to main menu
+    }
+
+    public void ReplayLevel()
+    {
+        // Replay current level
+    }
+
+    public void NextLevel()
+    {
+        // Go to next level
+    }
+}
