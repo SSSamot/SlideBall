eac1bc24bb187628fe931d6b4f79421ab79f5984
LenyNICOLAS <lnicolas@gaming.tech>
2023-05-17 17:57:23 +0200

    reload scene tuto

--- Assets/_scripts/Gyroscope.cs
+++ Assets/_scripts/Gyroscope.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class Gyroscope : MonoBehaviour
 {
     [SerializeField]
-    private float movementSpeed = 5f; 
-
+    private float movementSpeed = 8f; // Movement speed in units per second
+    float accumulativeRotation = 0f;
+    // Start is called before the first frame update
     void Start()
     {
         Input.gyro.enabled = true;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Vector3 rotationRate = Input.gyro.rotationRateUnbiased;
+        // Get the gyroscope rotation rate
+        Vector3 rotationRate = -Input.gyro.rotationRateUnbiased;
 
-        float movement = rotationRate.z * movementSpeed * Time.deltaTime;
+        // Accumulate the rotation rate over time to be used in the movement calculation
+        accumulativeRotation += rotationRate.z * Time.deltaTime;
 
+        // Apply movement speed
+        float movement = accumulativeRotation * movementSpeed;
+
+        // Apply movement to the transform's position
         transform.Translate(new Vector3(movement, 0, 0), Space.World);
 
         //transform.position += new Vector3(0, rotationRate.x * movementSpeed * Time.deltaTime, 0); 
+
+        // Clamp the position to the screen bounds  
 
 
     }
